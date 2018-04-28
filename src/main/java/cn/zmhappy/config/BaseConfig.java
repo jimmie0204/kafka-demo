d3299e007770acc5ee4e4c3556b581f873ee1716
liyanping06 <liyanping06@meituan.com>
2018-04-28 10:44:37 +0800

    测试

--- src/main/java/cn/zmhappy/config/BaseConfig.java
+++ src/main/java/cn/zmhappy/config/BaseConfig.java
@@ -5,8 +5,8 @@ public class BaseConfig {
 
     private static final String zkAddr = "192.168.1.111:2181";
 
-    private static final String kafkaAddr = "192.168.1.111:9092,192.168.1.111:9093,192.168.1.111:9094";
-//    private static final String kafkaAddr = "192.168.1.125:9092";
+//    private static final String kafkaAddr = "192.168.1.111:9092,192.168.1.111:9093,192.168.1.111:9094";
+    private static final String kafkaAddr = "192.168.1.125:9092";
 
     public static String getZkAddr() {
         return zkAddr;
